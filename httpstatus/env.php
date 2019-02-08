af78081765317805cbd7adad17346276710c8840
Maelpena <34342829+Maelpena@users.noreply.github.com>
2019-02-08 12:35:32 +0100

    le tout

---
+++ httpstatus/env.php
@@ -0,0 +1,12 @@
+<?php
+	/*
+		Ce fichier défini les constantes modifiables et les options
+	*/
+
+	//On défini l'environment
+    $env = [
+        'ENV' => 'dev',
+        'SESSION_NAME' => 'descartes',
+    ];
+
+    
